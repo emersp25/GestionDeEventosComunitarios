aceed174663b6dc937268e2c4e430eab4dfe5745
EmersonSP <emersp20@gmail.com>
2025-10-03 21:56:00 -0600

    backend update
    broken plugins fixed

--- gestion-eventos-comunitario/backend/src/main/java/com/gestion_eventos_comunitarios/gestion_eventos_comunitarios/GestionEventosApplication.java
+++ gestion-eventos-comunitario/backend/src/main/java/com/gestion_eventos_comunitarios/gestion_eventos_comunitarios/GestionEventosApplication.java
@@ -6,10 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Aplicación principal de Gestión de Eventos Comunitarios
  * Arquitectura Hexagonal (Ports & Adapters)
- * 
- * @author Tu Nombre
- * @version 1.0.0
  */
+
 @SpringBootApplication
 public class GestionEventosApplication {
 
